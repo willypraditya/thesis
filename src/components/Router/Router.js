b39f1cbe3f110de93543ec9169c7f6038e9b5621
Willy <w.praditya@yahoo.com>
2020-06-25 19:37:45 +0700

    add not found page

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Redirect, Switch, Router } from "react-router-dom";
 import NavbarRouter from "./NavbarRouter";
-// import NotFoundComponent from "components/Global/NotFoundComponent";
 import history from "configs/history";
 
 import NEW_Login from "components/Pages/NEW_RegisterAndLogin/Login";
 import NEW_SignUp from "components/Pages/NEW_RegisterAndLogin/SignUp";
 import NEW_ForgotPin from "components/Pages/NEW_RegisterAndLogin/ForgotPin";
+import NotFound from "components/Pages/NotFound";
 
 const RouterFlow = () => {
   // startLocale();
@@ -29,11 +29,11 @@ const RouterFlow = () => {
           )}
         />
         <Route path="/dashboard" component={NavbarRouter} />
-        <Route exact path="/new-login" component={NEW_Login} />
-        <Route exact path="/new-join" component={NEW_SignUp} />
-        <Route exact path="/new-forgot" component={NEW_ForgotPin} />
+        <Route exact path="/login" component={NEW_Login} />
+        <Route exact path="/join" component={NEW_SignUp} />
+        <Route exact path="/forgot" component={NEW_ForgotPin} />
 
-        {/* <Route component={NotFoundComponent}></Route> */}
+        <Route component={NotFound}></Route>
       </Switch>
     </Router>
   );
